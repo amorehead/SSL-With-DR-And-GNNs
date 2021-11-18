6fad5d97e9df7a540841ba95b8df2281ba202595
tmwatchanan <tmwatchanan@gmail.com>
2021-11-18 17:37:45 -0600

    refactor: extract models

--- ssl_with_dr_and_gnns.py
+++ ssl_with_dr_and_gnns.py
@@ -8,19 +8,20 @@ from urllib.error import HTTPError
 import pytorch_lightning as pl
 # PyTorch
 import torch
-import torch.nn as nn
 # PyTorch Geometric
 import torch_geometric
-import torch_geometric.nn as geom_nn
 # PL callbacks
 from pytorch_lightning.callbacks import ModelCheckpoint
+
+from models import GNNModel, MLPModel, NodeLevelGNN
 
 AVAIL_GPUS = min(1, torch.cuda.device_count())
 BATCH_SIZE = 256 if AVAIL_GPUS else 64
 # Path to the folder where the datasets are/should be downloaded
-DATASET_PATH = '/home/alexm/data/'
+BASE_URL = '.'
+DATASET_PATH = f'{BASE_URL}/data/'
 # Path to the folder where the pretrained models are saved
-CHECKPOINT_BASE_PATH = '/home/alexm/savedmodels/'
+CHECKPOINT_BASE_PATH = f'{BASE_URL}/savedmodels/'
 CHECKPOINT_PATH = os.path.join(CHECKPOINT_BASE_PATH, "GNNs/")
 
 # Setting the seed
@@ -44,9 +45,6 @@ os.makedirs(CHECKPOINT_PATH, exist_ok=True)
 cora_dataset = torch_geometric.datasets.Planetoid(root=DATASET_PATH, name='Cora')
 citeseer_dataset = torch_geometric.datasets.Planetoid(root=DATASET_PATH, name='Citeseer')
 
-# Populate list of possible GNN models to use
-gnn_layer_by_name = {"GCN": geom_nn.GCNConv, "GAT": geom_nn.GATConv, "GraphConv": geom_nn.GraphConv}
-
 # For each file, check whether it already exists. If not, try downloading it.
 for file_name in pretrained_files:
     file_path = os.path.join(CHECKPOINT_PATH, file_name)
@@ -63,138 +61,6 @@ for file_name in pretrained_files:
                 " or contact the author with the full output including the following error:\n",
                 e,
             )
-
-
-class GNNModel(nn.Module):
-    def __init__(
-            self,
-            c_in,
-            c_hidden,
-            c_out,
-            num_layers=2,
-            layer_name="GCN",
-            dp_rate=0.1,
-            **kwargs,
-    ):
-        """
-        Args:
-            c_in: Dimension of input features
-            c_hidden: Dimension of hidden features
-            c_out: Dimension of the output features. Usually number of classes in classification
-            num_layers: Number of "hidden" graph layers
-            layer_name: String of the graph layer to use
-            dp_rate: Dropout rate to apply throughout the network
-            kwargs: Additional arguments for the graph layer (e.g. number of heads for GAT)
-        """
-        super().__init__()
-        gnn_layer = gnn_layer_by_name[layer_name]
-
-        layers = []
-        in_channels, out_channels = c_in, c_hidden
-        for l_idx in range(num_layers - 1):
-            layers += [
-                gnn_layer(in_channels=in_channels, out_channels=out_channels, **kwargs),
-                nn.ReLU(inplace=True),
-                nn.Dropout(dp_rate),
-            ]
-            in_channels = c_hidden
-        layers += [gnn_layer(in_channels=in_channels, out_channels=c_out, **kwargs)]
-        self.layers = nn.ModuleList(layers)
-
-    def forward(self, x, edge_index):
-        """
-        Args:
-            x: Input features per node
-            edge_index: List of vertex index pairs representing the edges in the graph (PyTorch geometric notation)
-        """
-        for layer in self.layers:
-            # For graph layers, we need to add the "edge_index" tensor as additional input
-            # All PyTorch Geometric graph layer inherit the class "MessagePassing", hence
-            # we can simply check the class type.
-            if isinstance(layer, geom_nn.MessagePassing):
-                x = layer(x, edge_index)
-            else:
-                x = layer(x)
-        return x
-
-
-class MLPModel(nn.Module):
-    def __init__(self, c_in, c_hidden, c_out, num_layers=2, dp_rate=0.1):
-        """
-        Args:
-            c_in: Dimension of input features
-            c_hidden: Dimension of hidden features
-            c_out: Dimension of the output features. Usually number of classes in classification
-            num_layers: Number of hidden layers
-            dp_rate: Dropout rate to apply throughout the network
-        """
-        super().__init__()
-        layers = []
-        in_channels, out_channels = c_in, c_hidden
-        for l_idx in range(num_layers - 1):
-            layers += [nn.Linear(in_channels, out_channels), nn.ReLU(inplace=True), nn.Dropout(dp_rate)]
-            in_channels = c_hidden
-        layers += [nn.Linear(in_channels, c_out)]
-        self.layers = nn.Sequential(*layers)
-
-    def forward(self, x, *args, **kwargs):
-        """
-        Args:
-            x: Input features per node
-        """
-        return self.layers(x)
-
-
-class NodeLevelGNN(pl.LightningModule):
-    def __init__(self, model_name, **model_kwargs):
-        super().__init__()
-        # Saving hyperparameters
-        self.save_hyperparameters()
-
-        if model_name == "MLP":
-            self.model = MLPModel(**model_kwargs)
-        else:
-            self.model = GNNModel(**model_kwargs)
-        self.loss_module = nn.CrossEntropyLoss()
-
-    def forward(self, data, mode="train"):
-        x, edge_index = data.x, data.edge_index
-        x = self.model(x, edge_index)
-
-        # Only calculate the loss on the nodes corresponding to the mask
-        if mode == "train":
-            mask = data.train_mask
-        elif mode == "val":
-            mask = data.val_mask
-        elif mode == "test":
-            mask = data.test_mask
-        else:
-            assert False, "Unknown forward mode: %s" % mode
-
-        loss = self.loss_module(x[mask], data.y[mask])
-        acc = (x[mask].argmax(dim=-1) == data.y[mask]).sum().float() / mask.sum()
-        return loss, acc
-
-    def configure_optimizers(self):
-        # We use SGD here, but Adam works as well
-        transfer_learning = True  # If transfer learning, use a much lower initial learning rate
-        lr = 1e-4 if transfer_learning else 1e-1
-        optimizer = torch.optim.SGD(self.parameters(), lr=lr, momentum=0.9, weight_decay=2e-3)
-        return optimizer
-
-    def training_step(self, batch, batch_idx):
-        loss, acc = self.forward(batch, mode="train")
-        self.log("train_loss", loss)
-        self.log("train_acc", acc)
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        _, acc = self.forward(batch, mode="val")
-        self.log("val_acc", acc)
-
-    def test_step(self, batch, batch_idx):
-        _, acc = self.forward(batch, mode="test")
-        self.log("test_acc", acc)
 
 
 def train_node_classifier(model_name, dataset, fine_tune, **model_kwargs):
